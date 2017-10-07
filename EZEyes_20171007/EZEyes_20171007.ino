8294081d2f14c1acb917e0fe4b4a990e8dac4d9e
X3msnake <x3msnake@gmail.com>
2017-10-07 13:25:17 +0100

    Update Project Breakdown Comments
    Added datasheets sensor information and methods to use to the code

--- EZEyes_20171007/EZEyes_20171007.ino
+++ EZEyes_20171007/EZEyes_20171007.ino
@@ -1,3 +1,6 @@
+// EZEyes - Blind Assistive Technology; CC.BY.SA altLab 2017
+// X3msnake, João Silva, Gustavo Funk, Fernando Carvalho
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -8,8 +11,26 @@ void loop() {
 
 }
 
-/*
- * Debounce - https://playground.arduino.cc/Code/Bounce
- * Constrain(x, a, b)
+/* -------------------- HARDWARE --------------------
+ * 
+ * MODELO 3D OCULOS
+ * https://grabcad.com/library/ray-ban-wayfarers-1
+ * 
+ * POWER SUPPLY DESIGN
+ * Battery - 2 LIR 2450 /
  * Low Dropout reg LP2980?
+ * 
+ * SENSOR - VL53L0X 
+ * BreakoutBoard -  https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/downloads
+ * Datasheet - https://cdn-learn.adafruit.com/assets/assets/000/037/547/original/en.DM00279086.pdf
+ * 
+ * 
+ * -------------------- SOFTWARE --------------------
+ * 
+ * BUTTON DEBOUNCING
+ * Debounce - https://playground.arduino.cc/Code/Bounce
+ * 
+ * DATA CONSTRAIN
+ * Constrain(x, a, b)
+ * 
  */
