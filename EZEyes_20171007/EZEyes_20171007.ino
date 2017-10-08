d70518a4f85a654f7dc79fe0db1367b6c3585f46
X3msnake <x3msnake@gmail.com>
2017-10-08 01:29:35 +0100

    update

--- EZEyes_20171007/EZEyes_20171007.ino
+++ EZEyes_20171007/EZEyes_20171007.ino
@@ -24,8 +24,10 @@ void loop() {
  * DISTANCE SENSOR - VL53L0X 
  * BreakoutBoard -  https://learn.adafruit.com/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout/downloads
  * Datasheet - https://cdn-learn.adafruit.com/assets/assets/000/037/547/original/en.DM00279086.pdf
+ * ST Notes on Multiple Sensor usage - https://www.pololu.com/file/download/VL53L0X-AN4846.pdf?file_id=0J1239
+ * Using Multiple Sensors - https://github.com/pololu/vl53l0x-arduino/issues/1
  * 
- * VIBRATION MOTOR (Phone Junk)
+ * VIBRATION MOTOR (Phone Junk) 140mah / 60mah
  * Typical: https://www.precisionmicrodrives.com/sites/default/files/datasheet-for-the-306-101-vibration-motor.original.pdf
  * Round: https://www.precisionmicrodrives.com/sites/default/files/310-101-datasheet.pdf
  * 
